1583fe66d399976b249ef40bc15beb7ce03b3b45
endj <edin.j.1992@gmail.com>
2022-02-17 22:00:12 +0100

    init

---
+++ src/main/java/se/edinjakupovic/storenforwardservice/StorenforwardServiceApplication.java
@@ -0,0 +1,13 @@
+package se.edinjakupovic.storenforwardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorenforwardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorenforwardServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/edinjakupovic/storenforwardservice/StorenforwardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.edinjakupovic.storenforwardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorenforwardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
